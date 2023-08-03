f5678bf892f0d5419fa470fa3a8a4fbb5f0e37d5
shraaavss <shravs2707@gmail.com>
2023-08-03 19:27:16 +0530

    styling

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,16 +4,17 @@ import "./cssFiles/Todo.css";
 export default function Todo({ task, toggleComplt, delTodo, editTodo }) {
 
   return (
-    <div className="Todo">
-      <p onClick={() => toggleComplt(task.id)}
-      className={`${task.completed ? "completed" : ""}`}>{task.task}</p>
+    <div
+      className={`Todo ${task.completed ? "completed" : "not-comp"}`}
+    >
+      <p onClick={() => toggleComplt(task.id)}>{task.task}</p>
 
       <div>
         <span onClick={() => editTodo(task.id)}
-        className="material-symbols-outlined">edit_note</span>
-        
+          className="material-symbols-outlined">edit_note</span>
+
         <span onClick={() => delTodo(task.id)}
-        className="material-symbols-outlined">delete</span>
+          className="material-symbols-outlined">delete</span>
       </div>
     </div>
   );
